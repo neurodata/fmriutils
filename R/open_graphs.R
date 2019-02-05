5f99829d8572d7d71b389a268ec041a851a91e09
Eric Bridgeford <ebridge2@jhu.edu>
2019-02-05 16:16:36 -0500

    updated open graphs

--- R/open_graphs.R
+++ R/open_graphs.R
@@ -5,35 +5,48 @@
 #'
 #' @import stringr
 #' @import igraph
-#' @param fnames [n] a vector of filenames, with separation by underscores IE, dataset_subject_run_(other information).ext. Alternatively, a path to a directory containing a collection of appropriately named files with extension as specified below.
-#' @param dataset_id="" [1] the dataset id in the filenames.
-#' @param atlas_id="" [1] the atlas id in the filenames.
-#' @param fmt="graphml" a parameter indicating the format for graphs to be read in as. Options are ['graphml', 'edgelist'].
-#' @param verbose=FALSE whether to print the iteration being loaded.
-#' @param rtype='list' the type of output to return. Options are 'list' and 'array'.
-#' @param flatten=FALSE a parameter to flatten the array if the rtype is set to 'array'.
+#' @param fnames either:
 #' \describe{
-#'   \item{TRUE}{If rtype == 'array', then returns an [n x r^2] array}
-#'   \item{FALSE}{If rtype == 'array', then returns an [n x r x r] array}
+#'     \item{\code{vector} or 1-d \code{array}}{a vector of filenames in the BIDs specification, with separation by underscores IE, dataset_subject_run_(other information).ext.}
+#'     \item{\code{character}}{a path to a directory containing a collection of appropriately named files with extension as specified below}
 #' }
-#' @return gr the graphs of the subjects. Size depends on the parameters specified.
-#' @return dataset [n] a vector of the dataset ids for each subject.
-#' @return subjects [n] the subject ids
-#' @return sessions [n] the run ids
-#' @return tasks [n] the task ids
-#' @return runs [n] the run ids
+#' @param dataset_id the dataset id in the filenames. Defaults to \code{""}.
+#' @param atlas_id the atlas id in the filenames. Defaults to \code{""}.
+#' @param fmt a parameter indicating the format for graphs to be read in as. Options are \code{'adj'} or \code{'elist'}. Defaults to \code{'elist'}:
+#' \describe{
+#'     \item{\code{'adj'}}{Graphs are stored as an adjacency matrix, with a single header line indicating the parcel identifier (ROI), and successive lines the adjacency matrix as a csv. Note that if all graphs are not of the same dimensionality and the \code{'array'} return-type argument is specified, an error will be thrown.}
+#'     \item{\code{'elist'}}{Graphs are stored in comma-separated value format. Note that isolated ROIs within individual subjects are included.}
+#' }
+#' @param verbose whether to print the iteration being loaded. Defaults to \code{TRUE}.
+#' @param rtyp the type of output to return. Options are \code{'list'} and \code{'array'}. Defaults to \code{'list'}.
+#' @param flatten a parameter to flatten the array if the rtype is set to \code{'array'}:
+#' \describe{
+#'   \item{TRUE}{returns an \code{[n x r^2]} array}
+#'   \item{FALSE}{returns an \code{[n x r x r]} array}
+#' }
+#' @param rem.diag whether to remove diagonal (self-connetions) from the graph. Defaults to \code{TRUE}.
+#' @return a list containing the following:
+#' \item{\code{graphs} the graphs of the subjects. Size depends on the parameters specified.}
+#' \item{dataset} a \code{[n]} vector of the dataset ids for each subject.}
+#' \item{\code{subjects} a code{[n]} vector of the subject ids}
+#' \item{\code{runs} a code{[n]} vector of the run ids}
+#' \item{\code{tasks} a code{[n]} vector of the task ids}
+#' \item{\code{runs} a code{[n]} vector of the run ids}
 #' @export
-fmriu.io.open_graphs <- function(fnames, dataset_id="", atlas_id="", fmt='graphml', verbose=FALSE, rtype='list', flatten=FALSE) {
-  if (! (fmt %in% c('graphml', 'edgelist'))) {
-    stop('You have passed an invalid format type. Options are: [\'graphml\', \'edgelist\'].')
+fmriu.io.open_graphs <- function(fnames, dataset_id="", atlas_id="",
+                                 fmt='elist', verbose=FALSE, rtype='list', flatten=FALSE,
+                                 rem.diag=TRUE) {
+  if (! (fmt %in% c('adj', 'elist'))) {
+    stop('You have passed an invalid format type. Options are: [\'adj\', \'elist\'].')
+  }
+
+  if (fmt == 'elist') {
+    fmt = 'ncol'
+    ext = "csv"
   }
 
   if (is.character(fnames)) {
-    fnames <- list.files(fnames, pattern=paste('\\.', fmt, sep=""), full.names=TRUE)
-  }
-
-  if (fmt == 'edgelist') {
-    fmt = 'ncol'
+    fnames <- list.files(fnames, pattern=paste('\\.', ext, sep=""), full.names=TRUE)
   }
 
   if (! (rtype %in% c('list', 'array'))) {
@@ -70,6 +83,9 @@ fmriu.io.open_graphs <- function(fnames, dataset_id="", atlas_id="", fmt='graphm
     if (is.igraph(tgr)) {
       tgr <- get.adjacency(tgr, type="both", attr="weight", sparse=FALSE) # convert graph to adjacency matrix
       tgr[is.nan(tgr)] <- 0  # missing entries substituted with 0s
+      if (rem.diag) {
+        diag(tgr) <- 0
+      }
       gr[[basename]] <-t(tgr)
       subjects[counter] <- str_extract(basename, 'sub(.?)+?(?=_)')
       sessions[counter] <- str_extract(basename, 'ses(.?)+?(?=_)')
@@ -102,27 +118,31 @@ fmriu.io.open_graphs <- function(fnames, dataset_id="", atlas_id="", fmt='graphm
 #' A utility to open graphs information from multiple datasets. Assumes data is formatted by the BIDs spec.
 #' This means that derivatives are named as follows: sub-[###]_task-[abcde]_ses-[###]_run-[###]_(generic info).file
 #'
-#' @param basepath: [1] a path to a properly spec'd directory. See the example in `fmriutils.html` for details.
-#' @param datasets: [d] the datasets as a vector. Note that we expect a folder `<basepath>/<dataset>` for `dataset in datasets`.
-#' @param atlases: [a] the atlases as a vector. Note that we expect a folder `<basepath>/<dataset>/<gname>/<atlas>` for `atlas in atlases`.
-#' @param gname='connectomes': the folder name in which graphs are stored. Suggested options are ['connectomes', 'graphs'].
-#' @param fmt="graphml" a parameter indicating the format for graphs to be read in as. Options are ['graphml', 'edgelist'].
+#' @param basepath a path to a properly spec'd directory. See the example in `fmriutils.html` for details.
+#' @param datasets the datasets as a vector. Note that we expect a folder `<basepath>/<dataset>` for `dataset in datasets`.
+#' @param modality the modality identifier. Note that we expect a folder `<basepath>/<dataset>/<modality>` for `dataset` in `datasets`.
+#' @param atlases the atlases as a vector. Note that we expect a folder `<basepath>/<dataset>/<modality>/<gname>/<atlas>` for `atlas in atlases`, `dataset` in `datasets`.
+#' @param gname the folder name in which graphs are stored. Defaults to \code{'roi-connectomes'}.
+#' @param fmt a parameter indicating the format for graphs to be read in as. Options are \code{'adj'} or \code{'elist'}. Defaults to \code{'elist'}.
 #' @param verbose=FALSE: whether to print the id of the scan being loaded.
-#' @param rtype='list': the type of output to return. Options are 'list' and 'array'. Note that if rtype is 'array', all of the graphs must be derived from parcellations with the same number of vertices.
-#' @param flatten=FALSE a parameter to flatten the array if the rtype is set to 'array'.
+#' @param rtyp the type of output to return. Options are \code{'list'} and \code{'array'}. Defaults to \code{'list'}.
+#' @param flatten a parameter to flatten the array if the rtype is set to \code{'array'}:
 #' \describe{
-#'   \item{TRUE}{If rtype == 'array', then returns an [n x r^2] array}
-#'   \item{FALSE}{If rtype == 'array', then returns an [n x r x r] array}
+#'   \item{TRUE}{returns an \code{[n x r^2]} array}
+#'   \item{FALSE}{returns an \code{[n x r x r]} array}
 #' }
-#' @return graphs: [[n]][nroi, nroi] the graphs loaded from the specified directory. Shape depends on the parameters specified.
-#' @return dataset: [n] a vector of the dataset ids for each subject.
-#' @return atlas: [n] a vector of the atlas ids for each subject.
-#' @return subjects: [n] the subject ids
-#' @return sessions: [n] the session ids
-#' @return tasks: [n] the task ids
-#' @return runs: [n] the run ids
+#' @param rem.diag a parameter for whether to remove the diagonal (self-connections) from the graph. Defaults to \code{TRUE}.
+#' @return a list containing the following:
+#' \item{\code{graphs} the graphs of the subjects. Size depends on the parameters specified.}
+#' \item{dataset} a \code{[n]} vector of the dataset ids for each subject.}
+#' \item{\code{subjects} a code{[n]} vector of the subject ids}
+#' \item{\code{runs} a code{[n]} vector of the run ids}
+#' \item{\code{tasks} a code{[n]} vector of the task ids}
+#' \item{\code{runs} a code{[n]} vector of the run ids}
 #' @export
-fmriu.io.collection.open_graphs <- function(basepath, datasets="", atlases="", gname='connectomes', fmt='graphml', verbose=FALSE, rtype='list', flatten=FALSE) {
+fmriu.io.collection.open_graphs <- function(basepath, datasets, modality, atlases, gname='roi-connectomes',
+                                            fmt='elist', verbose=FALSE, rtype='list', flatten=FALSE,
+                                            rem.diag=TRUE) {
   if (! (rtype %in% c('list', 'array'))) {
     stop('You have passed an invalid return type. Options are: [\'list\', \'array\'].')
   }
@@ -137,10 +157,11 @@ fmriu.io.collection.open_graphs <- function(basepath, datasets="", atlases="", g
 
   for (ds in datasets) {
     for (at in atlases) {
-      path <- file.path(basepath, ds, gname, at, fsep = "/")
+      path <- file.path(basepath, ds, modality, gname, at, fsep = "/")
+      print(path)
       signalobj <- fmriu.io.open_graphs(path, dataset_id = ds, atlas_id = at,
                                         verbose = verbose, rtype = 'list',
-                                        fmt=fmt)
+                                        fmt=fmt, rem.diag=rem.diag)
 
       gr <- append(gr, signalobj$graphs)
       subjects <- c(subjects, signalobj$subjects)
